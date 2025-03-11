478c982554cc18ce33001e077ce30e1d43d2a956
gtuckerjones <gjones28@murraystate.edu>
2025-03-10 19:50:45 -0500

    kjkbh

--- Player/player.gd
+++ Player/player.gd
@@ -3,23 +3,6 @@ extends CharacterBody2D
 @export var speed: int = 200
 @onready var animations = $AnimationPlayer
 var facing_left = false  
-@export var weapon_scene: PackedScene 
-var weapon
-
-func _ready():
-	weapon = weapon_scene.instantiate()
-	add_child(weapon)
-
-func equip_weapon(new_weapon: String):
-	if weapon:
-		weapon.equipped_weapon = new_weapon
-		weapon.update_weapon_sprite()
-		weapon.update_weapon_collision()
-
-func _unhandled_input(event):
-	if event is InputEventMouseButton and event.pressed:
-		if weapon:
-			weapon.attack()
 
 func handleInput():
 	var moveDirection = Input.get_vector("move_left", "move_right", "move_up", "move_down")
