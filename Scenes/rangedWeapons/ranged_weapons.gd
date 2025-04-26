bbf294a9ceba1a46f8676bb85ae51b56db1fe6d5
gtuckerjones <gjones28@murraystate.edu>
2025-04-26 13:39:21 -0500

    rangedWeapons updates

--- Scenes/rangedWeapons/ranged_weapons.gd
+++ Scenes/rangedWeapons/ranged_weapons.gd
@@ -2,6 +2,17 @@ extends Node2D
 
 const BULLET = preload("res://Scenes/Bullet/bullet.tscn")
 @onready var muzzle: Marker2D = $Marker2D
+@onready var revolver = $Revolver
+@onready var shotgun = $Shotgun
+@onready var rifle = $Rifle
+@onready var tommygun = $Tommygun
+var current_weapon = ""
+
+func selectedGun():
+	revolver.visible = current_weapon == "revolver"
+	shotgun.visible = current_weapon == "shotgun"
+	rifle.visible = current_weapon == "rifle"
+	tommygun.visible = current_weapon == "tommygun"
 
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
@@ -12,9 +23,10 @@ func _process(delta: float) -> void:
 	else:
 		scale.y = .25
 		
+	selectedGun()
+		
 	if Input.is_action_just_pressed("fire"):
 		var bullet_instance = BULLET.instantiate()
 		get_tree().root.add_child(bullet_instance)
 		bullet_instance.global_position = muzzle.global_position
 		bullet_instance.rotation = rotation
-		
