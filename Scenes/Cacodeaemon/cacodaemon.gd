13add9b027d2a6e07a2e03d439e879e8e4ed0eaf
MJONES155 <mjones155@murraystate.edu>
2025-04-28 02:08:29 -0500

    Cacodaemon movement

--- Scenes/Cacodeaemon/cacodaemon.gd
+++ Scenes/Cacodeaemon/cacodaemon.gd
@@ -1,1 +1,32 @@
-extends RigidBody2D
+extends CharacterBody2D
+
+
+var speed = 55
+var player_chase = false
+var player = null
+
+func _physics_process(delta):
+	if player_chase:
+		if position.distance_to(player.position) > 10:
+			position+=(player.position-position)/speed
+			$AnimatedSprite2D.play("attack")
+			#move_and_collide(Vector2(0,0))
+		
+			if(player.position.x - position.x) < 0:
+				$AnimatedSprite2D.flip_h = true
+			else:
+				$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.play("idle")
+	move_and_slide()
+	
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	player = body
+	player_chase = true
+	
+
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	player = null
+	player_chase = false
